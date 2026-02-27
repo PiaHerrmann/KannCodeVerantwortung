458bcbb94e7b8d6e1337f0b4e843abdb627d2149
PiaHerrmann <pia.herrmann@tu-dortmund.de>
2026-02-27 12:10:13 +0100

    Permutations Test

--- zweiteErhebung/Datenauswertung2/Tests.R
+++ zweiteErhebung/Datenauswertung2/Tests.R
@@ -1,14 +1,22 @@
 print(setwd(dirname(rstudioapi::getSourceEditorContext()$path)))
 source("Gesamtauswertung.R")
-library(reshape2)
-library(ggplot2)
+library(dplyr)
 library(stringr)
 
 # chi^2- Test
 # H0 :Die Verteilung der Wenn‑Dann‑Konstruktionen ist unabhängig von der Partei.
-library(dplyr)
-library(stringr)
+konstr_counts <- long_konstr %>% count(Partei, Typ_Label)
+tab <- table(long_konstr$Partei, long_konstr$Typ_Label)
+chi_test <- chisq.test(tab)
+chi_test
+# 
+# 	Pearson's Chi-squared test
+# 
+# data:  tab
+# X-squared = 437.26, df = 49, p-value < 2.2e-16
+#-> H_0 wird abgelehnt
 
+# Kategorien zusammenfassen
 long_konstr_reduced <- long_konstr %>%
   mutate(Typ_Label = case_when(
     str_detect(Typ_Label, "uneindeutig") ~ "Sonstige",
@@ -22,28 +30,27 @@ tab_reduced <- table(long_konstr_reduced$Partei,
 
 chi_test_reduced <- chisq.test(tab_reduced)
 chi_test_reduced
-fischer=fisher.test(tab)
-chi_test
-# 
-# 	Pearson's Chi-squared test
-# 
-# data:  tab
-# X-squared = 437.26, df = 49, p-value < 2.2e-16
-#-> H_0 wird abgelehnt: 
-#--> Die Verteilung der Wenn‑Dann‑Konstruktionen
-# unterscheidet sich signifikant zwischen den Parteien.
 
-resid <- chi_test$stdres   # standardisierte Residuen pro Zelle
-round(resid, 2)
-resid_long <- melt(resid)
-ggplot(resid_long,
-       aes(x=Var1, y=Var2, fill=value)) +
-  geom_tile() +
-  scale_fill_gradient2(low="blue", mid="white", high="red", midpoint=0) +
-  labs(title="Abweichungen im Chi²-Test: Wenn-Dann-Konstruktionen nach Partei",
-       x="Partei", y="Konstruktionstyp", fill="Abweichung") +
-  theme_minimal(base_size=13) +
-  theme(axis.text.x=element_text(angle=45,hjust=1))
+# Erwartete Häufigkeiten sind zu klein
+
+# Permuationversion chi^2- Test
+# H0 :Die Verteilung der Wenn‑Dann‑Konstruktionen ist unabhängig von der Partei.
+set.seed(123)
+pvals <- c(
+  chisq.test(tab, simulate.p.value=TRUE, B=1000)$p.value,
+  chisq.test(tab, simulate.p.value=TRUE, B=5000)$p.value,
+  chisq.test(tab, simulate.p.value=TRUE, B=10000)$p.value,
+  chisq.test(tab, simulate.p.value=TRUE, B=50000)$p.value
+)
+names(pvals) <- c("B=1k","B=5k","B=10k","B=50k")
+pvals
+#        B=1k        B=5k       B=10k 
+# 9.99001e-04 1.99960e-04 9.99900e-05 
+#       B=50k 
+# 1.99996e-05
+#-> H_0 wird abgelehnt
+#-> Die Verteilung der Wenn‑Dann‑Konstruktionen
+# unterscheidet sich signifikant zwischen den Parteien.
 
 # testen nach Person
 long_konstr_persona <- daten %>%
@@ -69,7 +76,4 @@ chi_test_persona
 # X-squared = 241.88, df = 224, p-value = 0.1964
 # H_0 kann nicht abgelehnt werden
 
-#ANOVA
-anova_model <- aov(Ausf ~ KI + Land + Persona, data=daten)
-summary(anova_model)
 
