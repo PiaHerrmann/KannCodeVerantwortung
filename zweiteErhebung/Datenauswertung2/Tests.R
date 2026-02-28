f76fa36dd64e07e8d35b436d5bde100ec39a699b
PiaHerrmann <pia.herrmann@tu-dortmund.de>
2026-02-28 17:23:31 +0100

    im test nur preprocess und plot hinzugefügt

--- zweiteErhebung/Datenauswertung2/Tests.R
+++ zweiteErhebung/Datenauswertung2/Tests.R
@@ -1,7 +1,48 @@
 print(setwd(dirname(rstudioapi::getSourceEditorContext()$path)))
-source("Gesamtauswertung.R")
+source("preprocessing.R")
 library(dplyr)
 library(stringr)
+library(ggplot2)
+library(reshape2)
+
+#Daten vorbereiten
+# Konstruktionen – Wenn-dann-Analysen
+long_konstr <- daten %>%
+  select(starts_with("K_")) %>%
+  rename_with(~ str_remove_all(., "^K_"), starts_with("K_")) %>%   # entfernt "K_"
+  pivot_longer(
+    cols = everything(),
+    names_to = "Partei",
+    values_to = "Konstruktion"
+  ) %>%
+  separate_rows(Konstruktion, sep = ",") %>%
+  mutate(Konstruktion = str_trim(Konstruktion)) %>%
+  
+  # Typ-ID und Label trennen 
+  separate(
+    Konstruktion,
+    into = c("Typ_ID", "Typ_Label"),
+    sep = " - ",
+    convert = TRUE,
+    fill = "right"
+  ) %>%
+  
+  # Kategorie 1 ("keine Wenn-dann-Konstruktion") ausschließen
+  filter(Typ_ID != 1) %>%
+  
+  # Labels vereinheitlichen / kürzen
+  mutate(Typ_Label = case_when(
+    str_detect(Typ_Label, "in anderem Kontext") ~ "anderer Kontext",
+    str_detect(Typ_Label, "Migration") ~ "Migrationspolitik",
+    str_detect(Typ_Label, "Sozialpolitik") ~ "Sozialpolitik",
+    str_detect(Typ_Label, "Umweltpolitik") ~ "Umweltpolitik",
+    str_detect(Typ_Label, "Wirtschaftspolitik") ~ "Wirtschaftspolitik",
+    str_detect(Typ_Label, "Verkehrspolitik") ~ "Verkehrspolitik",
+    str_detect(Typ_Label, "(verweigert|ChatBot verweigert)") ~ "verweigert Aussage",
+    TRUE ~ Typ_Label
+  ))
+
+
 
 # chi^2- Test
 # H0 :Die Verteilung der Wenn‑Dann‑Konstruktionen ist unabhängig von der Partei.
@@ -33,15 +74,24 @@ chi_test_reduced
 
 # Erwartete Häufigkeiten sind zu klein
 
-# Permuationversion chi^2- Test
+# Monte Carlo chi^2- Test
 # H0 :Die Verteilung der Wenn‑Dann‑Konstruktionen ist unabhängig von der Partei.
 set.seed(123)
-chisq.test(tab, simulate.p.value=TRUE, B=50000)
-
-# 1.99996e-05
+chi_test=chisq.test(tab, simulate.p.value=TRUE, B=50000)
+chi_test$p.value
+# p-value = 1.99996e-05
 #-> H_0 wird abgelehnt
 #-> Die Verteilung der Wenn‑Dann‑Konstruktionen
 # unterscheidet sich signifikant zwischen den Parteien.
+
+resid <- chi_test$stdres
+resid_long <- melt(resid)
+ggplot(resid_long, aes(x=Var1, y=Var2, fill=value)) +
+  geom_tile() +
+  scale_fill_gradient2(low="blue", mid="white", high="red", midpoint=0) +
+  labs(title="Standardisierte Residuen des Chi²-Tests",
+       x="Partei", y="Wenn-Dann-Konstruktion", fill="Abweichung") +
+  theme_minimal()
 
 # testen nach Person
 long_konstr_persona <- daten %>%
